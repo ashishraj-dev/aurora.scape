bdf5f8b400273a6e8029c6444b74eb8f104554d7
Ashish <ashishr@profiledraft.com>
2025-02-22 00:39:39 +0530

    added aos library

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+AOS.init();
+
+// You can also pass an optional settings object
+// below listed default settings
+AOS.init({
+  // Global settings:
+  disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+  startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+  initClassName: 'aos-init', // class applied after initialization
+  animatedClassName: 'aos-animate', // class applied on animation
+  useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+  disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+  debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+  throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+
+  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+  offset: 120, // offset (in px) from the original trigger point
+  delay: 0, // values from 0 to 3000, with step 50ms
+  duration: 400, // values from 0 to 3000, with step 50ms
+  easing: 'ease', // default easing for AOS animations
+  once: false, // whether animation should happen only once - while scrolling down
+  mirror: false, // whether elements should animate out while scrolling past them
+  anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+});
+
 const toggleBtn = document.querySelector('.btn-mobile-nav');
 const menuIcon = document.querySelector(
   '.icon-mobile-nav[name="menu-outline"]'
@@ -150,6 +175,10 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   });
 
+  if (window.scrollY > wrapperHeight) {
+    scrollTopBtn.classList.add('show-scrollTop');
+  }
+
   // Scroll effect for header
   window.addEventListener('scroll', function () {
     // const logo = document.querySelector(".logo");
@@ -175,14 +204,22 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 
   window.addEventListener('scroll', function () {
+    const ripple = document.querySelector('.ripple');
+
     if (window.scrollY > wrapperHeight) {
       marquee.forEach(item => {
         item.classList.add('anim-paused');
       });
+      if (ripple) {
+        ripple.classList.remove('anim-paused');
+      }
     } else {
       marquee.forEach(item => {
         item.classList.remove('anim-paused');
       });
+      if (ripple) {
+        ripple.classList.add('anim-paused');
+      }
     }
   });
 
